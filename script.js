12dc81af4ecf18c94873e599405ffd131cb1191e
shayne243 <marques0@purdue.edu>
2020-06-13 01:39:41 -0400

    Managed grid size by js instead of css

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let newDiv = null;
 let rows = 16;
 let cols = 16;
 function generateGrid() {
+    container.setAttribute("style", "grid-template-columns: repeat(" + cols + ",auto); grid-template-rows: repeat(" + rows + ",auto);")
     for(let i = 0; i < (rows * cols); ++i) {
         newDiv = document.createElement("div");
         newDiv.setAttribute("class", "box");
@@ -16,6 +17,16 @@ function clearGrid() {
     for (let i = 0; i < grid.length; i++) {
         grid[i].style.background = "aqua";
     }
+}
+function shrinkGrid() {
+    rows *= 0.5;
+    cols *= 0.5;
+    generateGrid();
+}
+function growGrid() {
+    rows *= 2;
+    cols *= 2;
+    generateGrid();
 }
 generateGrid();
 const clearbtn = document.getElementById("clear");
