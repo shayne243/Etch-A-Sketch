68eca29de975ad81634f6080c697a39820c62f87
shayne243 <marques0@purdue.edu>
2020-06-13 01:28:57 -0400

    Clear function done

--- script.js
+++ script.js
@@ -2,10 +2,21 @@ const container = document.getElementById("maincontainer");
 let newDiv = null;
 let rows = 16;
 let cols = 16;
-for(let i = 0; i < (rows * cols); ++i) {
+function generateGrid() {
+    for(let i = 0; i < (rows * cols); ++i) {
         newDiv = document.createElement("div");
         newDiv.setAttribute("class", "box");
         newDiv.addEventListener("mouseover", function color() {this.style.background = "black";
         });
         container.appendChild(newDiv);
+    }
 }
+function clearGrid() {
+    let grid = document.querySelectorAll(".box");
+    for (let i = 0; i < grid.length; i++) {
+        grid[i].style.background = "aqua";
+    }
+}
+generateGrid();
+const clearbtn = document.getElementById("clear");
+clearbtn.addEventListener("click", clearGrid);
